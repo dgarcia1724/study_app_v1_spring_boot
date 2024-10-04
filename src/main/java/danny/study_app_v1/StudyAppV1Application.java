69bd8ee9d6e57d960c4bc1a9786fc42ad0a01022
Danny Garcia <dgarcia1724@berkeley.edu>
2024-10-04 11:58:31 -0700

    database connected

---
+++ src/main/java/danny/study_app_v1/StudyAppV1Application.java
@@ -0,0 +1,13 @@
+package danny.study_app_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyAppV1Application.class, args);
+	}
+
+}
